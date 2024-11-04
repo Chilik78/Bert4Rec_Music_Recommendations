51f2657e2232ddd5ec97b39dc9af7a963d08e09b
lizard222 <rustammattiev21@gmail.com>
2024-11-04 15:11:29 +0300

    Добавил фиусы сервака.

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
 from server.api_routes import *
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
 app = FastAPI()
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
 
 
 app.include_router(user_router, prefix='/users')
 app.include_router(music_router, prefix='/music')
 app.include_router(history_router, prefix='/history')
+
+uvicorn.run(app)
